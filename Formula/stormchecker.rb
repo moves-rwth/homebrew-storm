582c2f9f38a970272f87a4e1358c3c5f6099e350
dehnert <dehnert@cs.rwth-aachen.de>
2017-02-05 21:51:31 +0100

    maybe this time parallel

--- Formula/stormchecker.rb
+++ Formula/stormchecker.rb
@@ -16,10 +16,8 @@ class Stormchecker < Formula
   def install
     ENV.deparallelize  # if your formula fails when building in parallel
 
-    system "mkdir", "build"
-    system "cd", "build"
     system "cmake", ".", "-DSTORM_DEVELOPER=OFF", "-DCMAKE_BUILD_TYPE=RELEASE", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DSTORM_FORCE_SHIPPED_CARL=ON"
-    system "make", "-j#{ENV.HOMEBREW_MAKE_JOBS}", "install" # if this fails, try separate make/make install steps
+    system "make", "-j#{ENV.determine_make_jobs}", "install" # if this fails, try separate make/make install steps
   end
 
   test do
