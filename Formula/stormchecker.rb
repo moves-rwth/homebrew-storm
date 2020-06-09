602182f9fdccb3eb7ba43d317b048ec933978338
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2020-06-09 18:00:05 +0200

    Fixed issues raised in brew audit

--- Formula/stormchecker.rb
+++ Formula/stormchecker.rb
@@ -10,20 +10,20 @@ class Stormchecker < Formula
   option "with-tbb", "Build Storm with Intel Thread Building Blocks (TBB) support."
   option "with-cocoalib", "Build with support for CoCoALib (also requires CArl to be built with support for CoCoALib)."
 
-  depends_on :macos => :mavericks
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "boost"
-  depends_on "gmp"
-  depends_on "z3"
-  depends_on "automake" => :build
-  depends_on "autoconf" => :build
-  depends_on "xerces-c"
-  depends_on "tbb" if build.with?("tbb")
-  depends_on "glpk"
-  depends_on "hwloc"
   depends_on "doxygen"
-  depends_on "moves-rwth/misc/cocoalib" if build.with?("cocoalib")
+  depends_on "glpk"
+  depends_on "gmp"
+  depends_on "hwloc"
+  depends_on :macos => :mavericks
   depends_on "moves-rwth/misc/carl" => build.with?("cocoalib") ? ["with-thread-safe", "with-cln", "with-ginac", "with-cocoalib"] : ["with-thread-safe", "with-cln", "with-ginac"]
+  depends_on "xerces-c"
+  depends_on "z3"
+  depends_on "moves-rwth/misc/cocoalib" => :optional
+  depends_on "tbb" => :optional
 
   def install
     args = %w[
