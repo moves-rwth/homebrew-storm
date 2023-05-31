e901332798445d1e6409c54f351fec772b3c9dc4
Matthias Volk <m.volk@utwente.nl>
2023-05-31 23:26:01 +0200

    Bump Storm's homebrew formula to 1.8.0 and update formula

--- Formula/stormchecker.rb
+++ Formula/stormchecker.rb
@@ -1,28 +1,25 @@
 class Stormchecker < Formula
   desc "A modern probabilistic model checker"
   homepage "https://www.stormchecker.org"
-  url "https://github.com/moves-rwth/storm/archive/1.7.0.tar.gz"
-  version "1.7.0"
-  sha256 "e3ab398a6497059058408b6d09108e7c224da7fe1515c9e57f63b7e1fd95399c"
-  head "https://github.com/moves-rwth/storm.git", :using => :git
+  url "https://github.com/moves-rwth/storm/archive/1.8.0.tar.gz"
+  version "1.8.0"
+  sha256 "ea4467dfc3e984b7b707d2d8f73988049e9ca5f4489738fa067caa0ba0796e35"
+  head "https://github.com/moves-rwth/storm.git", using: :git, branch: "master"
 
   # option "with-single-thread", "Build Storm using just one thread."
   option "with-tbb", "Build Storm with Intel Thread Building Blocks (TBB) support."
-  option "with-cocoalib", "Build with support for CoCoALib (also requires CArl to be built with support for CoCoALib)."
   option "with-spot", "Build Storm with Spot (required for LTL model checking)."
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "boost"
-  depends_on "doxygen"
   depends_on "glpk"
   depends_on "gmp"
   depends_on "hwloc"
-  depends_on "moves-rwth/misc/carl" => build.with?("cocoalib") ? ["with-thread-safe", "with-cln", "with-ginac", "with-cocoalib"] : ["with-thread-safe", "with-cln", "with-ginac"]
+  depends_on "moves-rwth/misc/carl-storm" => ["with-cln", "with-ginac"]
   depends_on "xerces-c"
   depends_on "z3"
-  depends_on "moves-rwth/misc/cocoalib" => :optional
   depends_on "spot" => :optional
   depends_on "tbb" => :optional
 
