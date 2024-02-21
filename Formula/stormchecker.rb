3015f51ebf4a4c4ee688f9ee960052f036703fd9
Matthias Volk <m.volk@tue.nl>
2024-02-21 14:15:31 +0100

    Updated stormchecker formula

--- Formula/stormchecker.rb
+++ Formula/stormchecker.rb
@@ -1,9 +1,11 @@
 class Stormchecker < Formula
   desc "A modern probabilistic model checker"
   homepage "https://www.stormchecker.org"
-  url "https://github.com/moves-rwth/storm/archive/1.8.1.tar.gz"
-  version "1.8.1"
+  # version is extracted from url
+  url "https://github.com/moves-rwth/storm/archive/refs/tags/1.8.1.tar.gz"
   sha256 "13de6e7816f2b796db3557ac6b058e2ccab9cd129e243cfce93dd7cdd82f3ee1"
+  revision 1 # Formula updated but not source
+  license "GPL-3.0-only"
   head "https://github.com/moves-rwth/storm.git", using: :git, branch: "master"
 
   # option "with-single-thread", "Build Storm using just one thread."
@@ -17,16 +19,16 @@ class Stormchecker < Formula
   depends_on "glpk"
   depends_on "gmp"
   depends_on "hwloc"
-  on_intel do
-    depends_on "moves-rwth/misc/carl-storm" => ["with-cln", "with-ginac"]
-  end
-  on_arm do
-    depends_on "moves-rwth/misc/carl-storm"
-  end
   depends_on "xerces-c"
   depends_on "z3"
   depends_on "spot" => :optional
   depends_on "tbb" => :optional
+  on_arm do
+    depends_on "moves-rwth/storm/carl-storm"
+  end
+  on_intel do
+    depends_on "moves-rwth/storm/carl-storm" => ["with-cln", "with-ginac"]
+  end
 
   def install
     args = %w[
