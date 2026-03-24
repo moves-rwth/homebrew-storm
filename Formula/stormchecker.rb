7c1d56be3c621d709131736c5b118dc76340b175
Matthias Volk <volkm@users.noreply.github.com>
2026-03-24 18:32:35 +0100

    Revised formula (#17)

--- Formula/stormchecker.rb
+++ Formula/stormchecker.rb
@@ -1,17 +1,11 @@
 class Stormchecker < Formula
-  desc "A modern probabilistic model checker"
+  desc "Modern probabilistic model checker"
   homepage "https://www.stormchecker.org"
-  # version is extracted from url
-  url "https://github.com/moves-rwth/storm/archive/refs/tags/1.12.0.tar.gz"
+  url "https://github.com/stormchecker/storm/archive/refs/tags/1.12.0.tar.gz"
   sha256 "7bfda9c2f8189391c1fe37893bb5b4a36642fca6aebc3d4fa7db5574f5ed8f5b"
   license "GPL-3.0-only"
   head "https://github.com/moves-rwth/storm.git", using: :git, branch: "master"
 
-  # option "with-single-thread", "Build Storm using just one thread."
-  option "with-tbb", "Build Storm with Intel Thread Building Blocks (TBB) support."
-  option "with-spot", "Build Storm with Spot (required for LTL model checking)."
-
-  depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "boost"
@@ -21,26 +15,52 @@ class Stormchecker < Formula
   depends_on "gmp"
   depends_on "hwloc"
   depends_on "libarchive"
+  depends_on "spot"
   depends_on "xerces-c"
   depends_on "z3"
-  depends_on "spot" => :optional
-  depends_on "tbb" => :optional
+
+  # Additional dependencies (usually obtained via FetchContent)
+  resource "carl-storm" do
+    url "https://github.com/stormchecker/carl-storm/archive/refs/tags/14.34.tar.gz"
+    sha256 "39070125f8bd872efa0b5b04067dfe6258e3317bae19cc6e196dd740f2de3fcc"
+  end
 
   def install
+    # Stage resources
+    (buildpath/"fetched_deps").mkpath
+    resources.each do |r|
+      r.stage buildpath/"fetched_deps"/r.name
+    end
+
+    # Set CMake flags
     args = %w[
       -DCMAKE_BUILD_TYPE=RELEASE
       -DSTORM_COMPILE_WITH_CCACHE=OFF
       -DSTORM_BUILD_TESTS=OFF
+      -DFETCHCONTENT_SOURCE_DIR_CARL=fetched_deps/carl-storm
+      -DFETCHCONTENT_SOURCE_DIR_SYLVANFETCH=resources/3rdparty/sylvan
     ]
-    args << "-DSTORM_USE_INTELTBB=ON" if build.with?("tbb")
 
-    mktemp do
-      system "cmake", buildpath, *(std_cmake_args + args)
-      system "make", "install"
-    end
+    # Build and install
+    system "cmake", "-S", ".", "-B", "build", *(std_cmake_args + args)
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    shell_output("#{bin}/storm", 1)
+    # Write small MDP file
+    (testpath/"walk.nm").write <<~EOS
+      mdp
+      const int N;
+      const double p = 0.5;
+      module main
+              x : [0..N] init N/2;
+              [right] x<N -> p : (x'=x+1) + (1-p) : (x'=x);
+              [left] x>0 -> p : (x'=x-1) + (1-p) : (x'=x);
+      endmodule
+    EOS
+    # Run Storm and check output
+    output = shell_output("#{bin}/storm --prism #{testpath}/walk.nm -prop 'Pmax=? [F<=10 x=0]' -const 'N=10' --exact")
+    assert_match "Result (for initial states): 319/512", output
   end
 end
