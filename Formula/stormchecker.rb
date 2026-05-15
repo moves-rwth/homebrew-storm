d6b50dac710e63898bb27cec7b3da077935e21d8
Matthias Volk <24915841+volkm@users.noreply.github.com>
2026-05-15 14:21:32 +0000

    stormchecker: add 1.13.0 bottle.

--- Formula/stormchecker.rb
+++ Formula/stormchecker.rb
@@ -6,6 +6,15 @@ class Stormchecker < Formula
   license "GPL-3.0-only"
   head "https://github.com/stormchecker/storm.git", using: :git, branch: "master"
 
+  bottle do
+    root_url "https://github.com/moves-rwth/homebrew-storm/releases/download/stormchecker-1.13.0"
+    sha256 cellar: :any,                 arm64_tahoe:   "8cb9384e08b2fedf0f7248e67aff657a551d67b8e39656975e4e13d8c662c988"
+    sha256 cellar: :any,                 arm64_sequoia: "3971d9a0719882e90972fb7d4808fc65bb53ea4f01c40769890b5a3fc2cff2a6"
+    sha256 cellar: :any,                 arm64_sonoma:  "3c175a62a2c6523d7517686a6562e1b71942a70cdbe9e402c19bf9fd92d72f7c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ccc22618bed8965ffcab8769883b8c93c21179fa53ea713b7d77c0fdfcf5a205"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7079b7672c30f1092b706efd476861409fce12bcbdfda2c608f6a835437d6e48"
+  end
+
   depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "boost"
